e90e4cb3446638df846ca7683851ef14a66302b0
root <root@looking-far.(none)>
2014-01-10 20:12:13 -0500

    change

--- app/connections/twitter.js
+++ app/connections/twitter.js
@@ -9,7 +9,7 @@ var twitterConnection = function (app){
 	passport.use( new TwitterStrategy({
 			consumerKey :'I1wtPCSNg61NdDKX7cEg',
 			consumerSecret :'W3oJdWoevKgNbeiGh6qvZjiKRH1ePhhNDFN4AaG28E',
-			callbackURL: 'http://127.0.0.1:3000/auth/twitter/callback'
+			callbackURL: '/auth/twitter/callback'
 		},
 		function (token, tokenSecret, profile, done){
 			//debugger;
