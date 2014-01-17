9166dab33f44caa25f3cf87c78bcb8660ff768db
WalterC87 <wdlara.8789@gmail.com>
2014-01-16 22:23:38 -0600

    responsive design

--- app/connections/facebook.js
+++ app/connections/facebook.js
@@ -7,8 +7,8 @@ var facebookConnection = function (app){
 	passport.use(new FacebookStrategy({
 			clientID: '340114766126931',
 			clientSecret : 'd133267b60216f70fe252e2c630c601b',
-			callbackURL: 'http://looking-far.com/auth/facebook/callback',
-			//callbackURL: 'http://127.0.0.1:3000/auth/facebook/callback',
+			//callbackURL: 'http://looking-far.com/auth/facebook/callback',
+			callbackURL: 'http://127.0.0.1:3000/auth/facebook/callback',
 			profileFields: ['id','displayName','photos']
 		},
 
