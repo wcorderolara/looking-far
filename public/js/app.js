3075b70cd1abc0d2e0e610ba2bb74bd89f2b9464
WalterC87 <wdlara.8789@gmail.com>
2014-02-04 00:12:20 -0600

    upgrade looking-far

--- public/js/app.js
+++ public/js/app.js
@@ -66,9 +66,9 @@ $('#btnpost_network').on('click',function (){
 })
 
 $('#btnpost').on('click',function (){
-	$('#fear').html($('#fear_post').val());
-	$('#aspiration').html($('#aspiration_post').val());
-	$('#regreat').html($('#regret_post').val());
+	$('#fear_single').html($('#fear_post').val());
+	$('#aspiration_single').html($('#aspiration_post').val());
+	$('#regreat_single').html($('#regret_post').val());
 })
 
 $('#fear_post').on('blur',function (e){
@@ -125,6 +125,187 @@ $('#regret').on('blur',function (e){
 	}
 })
 
+function validarEmail( email ) {
+    expr = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    if ( !expr.test(email) ){
+    	return false;
+    }
+    return true;
+}
+
+$('#email').on('blur', function (e){
+	e.preventDefault();
+	if( validarEmail($(this).val()) === false ){
+		$(this).siblings('p').css('display','block');
+		$(this).focus();
+	}else{
+		$(this).siblings('p').css('display','none');
+	}
+})
+
+$('#fear_single').on('blur', function (e){
+	e.preventDefault();
+	if( $(this).val() === "" ){
+		$(this).siblings('p').css('display','block');
+		$(this).focus();
+	}else{
+		$(this).siblings('p').css('display','none');
+	}
+})
+
+$('#aspiration_single').on('blur', function (e){
+	e.preventDefault();
+	if( $(this).val() === "" ){
+		$(this).siblings('p').css('display','block');
+		$(this).focus();
+	}else{
+		$(this).siblings('p').css('display','none');
+	}
+})
+
+$('#regreat_single').on('blur', function (e){
+	e.preventDefault();
+	if( $(this).val() === "" ){
+		$(this).siblings('p').css('display','block');
+		$(this).focus();
+	}else{
+		$(this).siblings('p').css('display','none');
+	}
+})
+
+function validarForm(){
+	var valida = true;
+
+	if($('#email').val() === ''){
+		$('#email').siblings("p").css('display','block');
+		$('#email').focus();
+		valida = false;
+		return valida;
+	}else if($('#name').val() === ''){
+		$('#name').siblings("p").css('display','block');
+		$('#name').focus();
+		valida = false;
+		return valida;
+	}else if($('#city').val() === ''){
+		$('#city').siblings("p").css('display','block');
+		$('#city').focus();
+		valida = false;
+		return valida;
+	}else if($('#fear_single').val() === ''){
+		$('#fear_single').siblings("p").css('display','block');
+		$('#fear_single').focus();
+		valida = false;
+		return valida;
+	}else if($('#aspiration_single').val() === ''){
+		$('#aspiration_single').siblings("p").css('display','block');
+		$('#aspiration_single').focus();
+		valida = false;
+		return valida;
+	}else if($('#regreat_single').val() === ''){
+		$('#regreat_single').siblings("p").css('display','block');
+		$('#regreat_single').focus();
+		valida = false;
+		return valida;
+	}else{
+		valida = true;
+	}
+	return valida;
+}
+
+$('#btnpost_normal').on('click', function (e){
+ 	if(validarForm() === true){
+		$('#frmSinglePost').submit();
+	}else{
+		return false;
+	}
+})
+
+$('#email_social').on('blur', function (e){
+	e.preventDefault();
+	if( validarEmail($(this).val()) === false ){
+		$(this).siblings('p').css('display','block');
+		$(this).focus();
+	}else{
+		$(this).siblings('p').css('display','none');
+	}
+})
+
+$('#fear_social').on('blur', function (e){
+	e.preventDefault();
+	if( $(this).val() === "" ){
+		$(this).siblings('p').css('display','block');
+		$(this).focus();
+	}else{
+		$(this).siblings('p').css('display','none');
+	}
+})
+
+$('#aspiration_social').on('blur', function (e){
+	e.preventDefault();
+	if( $(this).val() === "" ){
+		$(this).siblings('p').css('display','block');
+		$(this).focus();
+	}else{
+		$(this).siblings('p').css('display','none');
+	}
+})
+
+$('#regreat_social').on('blur', function (e){
+	e.preventDefault();
+	if( $(this).val() === "" ){
+		$(this).siblings('p').css('display','block');
+		$(this).focus();
+	}else{
+		$(this).siblings('p').css('display','none');
+	}
+})
+
+function validaSocialForm(){
+	var valida = true;
+
+	if($('#email_social').val() === ''){
+		$('#email_social').siblings("p").css('display','block');
+		$('#email_social').focus();
+		valida = false;
+		return valida;
+	}else if($('#name_social').val() === ''){
+		$('#name_social').siblings("p").css('display','block');
+		$('#name_social').focus();
+		valida = false;
+		return valida;
+	}else if($('#city_social').val() === ''){
+		$('#city_social').siblings("p").css('display','block');
+		$('#city_social').focus();
+		valida = false;
+		return valida;
+	}else if($('#fear_social').val() === ''){
+		$('#fear_social').siblings("p").css('display','block');
+		$('#fear_social').focus();
+		valida = false;
+		return valida;
+	}else if($('#aspiration_social').val() === ''){
+		$('#aspiration_social').siblings("p").css('display','block');
+		$('#aspiration_social').focus();
+		valida = false;
+		return valida;
+	}else if($('#regreat_social').val() === ''){
+		$('#regreat_social').siblings("p").css('display','block');
+		$('#regreat_social').focus();
+		valida = false;
+		return valida;
+	}else{
+		valida = true;
+	}
+	return valida;
+}
+
+$('#btnpost_social').on('click', function (e){
+	if(validaSocialForm() === true){
+		$('#frmSocialPost').submit();
+	}else{
+		return false;
+	}
+})
 
 
 
